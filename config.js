d33cf433eab4b825ca6ee33d68b28952353ea062
VanBast077 <vanbast78@gmail.com>
2026-08-29 21:04:46 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+// API grátis que funciona no GitHub Pages sem erro de CORS
+const API_KEY = "free";
+const BASE_URL = "https://api.allorigins.win/raw?url=https://site.api.espn.com/apis/site/v2/sports/soccer/bra.1/scoreboard";
